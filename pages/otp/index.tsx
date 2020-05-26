95f0196344cf9f02d48a182628420319d8666ee3
bodisorn <puddingjang.haho@gmail.com>
2020-05-26 08:54:15 +0700

    update structure file , components

--- pages/otp/index.tsx
+++ pages/otp/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import Layout from '../../components/Layout';
 import Proloader from '../../components/Proloader';
-import HeroSectionV3 from '../../components/HeroSectionV3';
-import SmsSectionV3 from '../../components/SmsSectionV3';
-import SimpleIconSection from '../../components/SimpleIconSection';
-import TrackSection from '../../components/TrackSection';
-import CtaSectionV2 from '../../components/CtaSectionV2';
+import HeroSection from '../../components/otp/HeroSection';
+import SmsSection from '../../components/otp/SmsSection';
+import SimpleIconSection from '../../components/otp/SimpleIconSection';
+import TrackSection from '../../components/otp/TrackSection';
+import CtaSection from '../../components/otp/CtaSection';
 
 import BacktoTop from '../../components/BacktoTop';
 
@@ -15,11 +15,11 @@ export default function Home() {
     <Layout>
       <Proloader></Proloader>
       <div className="page_wrapper">
-        <HeroSectionV3></HeroSectionV3>
-        <SmsSectionV3></SmsSectionV3>
+        <HeroSection></HeroSection>
+        <SmsSection></SmsSection>
         <SimpleIconSection></SimpleIconSection>
         <TrackSection></TrackSection>
-        <CtaSectionV2></CtaSectionV2>
+        <CtaSection></CtaSection>
       </div>
       <BacktoTop></BacktoTop>
     </Layout>
