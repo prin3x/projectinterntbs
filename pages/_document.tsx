ae7c68db885bd22b4d43630eb0be5b033ce7750d
bodisorn <puddingjang.haho@gmail.com>
2020-06-05 13:10:20 +0700

    fixbug load jquery

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -122,13 +122,13 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
-          <Main />
-          <NextScript />
           <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
           <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.0/js/jquery.nice-select.min.js"></script>
+          <Main />
+          <NextScript />
           <script src="/js/main.js"></script>
         </body>
       </Html>
