13630f7c4b9b06cdcf35b0ed9deb3b21a0666a95
Phoowanon <phoowanon@1moby.com>
2020-07-07 17:10:13 +0700

    fix css v6

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,17 @@
 import Header from './Header';
 import Footer from './Footer';
-const Layout = (props: any) => (
-  <div>
-    <Header />
-    {props.children}
-    <Footer />
-  </div>
+import { i18n } from '../i18n';
+import React, { useEffect } from 'react';
+const Layout = (props: any) => {
+useEffect(() => {
+i18n.changeLanguage('th');
+}, []);
+return (
+<div>
+<Header />
+{props.children}
+<Footer />
+</div>
 );
+};
 export default Layout;
