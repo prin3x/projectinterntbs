ce9848f75230825e462bf7b20b3c77665b74139f
bodisorn <puddingjang.haho@gmail.com>
2020-07-24 11:35:30 +0700

    fix code

--- components/pricing/AllPricing.tsx
+++ components/pricing/AllPricing.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { useState } from 'react';
 
 const AllPricing = ({ t }: any) => {
-  const [dataToggle, setIsLogin] = useState([]);
+  const [dataToggle, setIsLogin] = useState([] as any);
   React.useEffect(() => {
     if (screen.width > 767) {
       setIsLogin([
