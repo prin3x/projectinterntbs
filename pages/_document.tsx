fc689072c556b410daef7f3cad001803d39785f6
bodisorn <puddingjang.haho@gmail.com>
2020-09-01 16:15:45 +0700

    fix bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,9 +42,12 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.0/css/nice-select.css"
             rel="stylesheet"
           />
-          <link href={`http://localhost:3000/css/style.css`} rel="stylesheet" />
           <link
-            href={`http://localhost:3000/css/responsive.css`}
+            href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/css/style.css`}
+            rel="stylesheet"
+          />
+          <link
+            href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/css/responsive.css`}
             rel="stylesheet"
           />
           <link
