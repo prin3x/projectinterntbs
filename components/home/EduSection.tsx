a7ac7030716a4bdfc09f4e779ee0149d696f50bb
Apichart Khairavi <apichart@1moby.com>
2020-09-23 13:43:54 +0700

    change text and link

--- components/home/EduSection.tsx
+++ components/home/EduSection.tsx
@@ -14,40 +14,46 @@ const EduSection = ({ t }: any) => (
           <div className="row align-items-center">
             <div className="col-lg-6 col-md-6 offset-md-3 offset-lg-0 col-sm-10 offset-sm-1">
               <div className="edu_box">
-                <img
-                  className="lazyload"
-                  data-src={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img/img_7.png`}
-                  alt="Image"
-                />
-                <div className="edu_box_text">
-                  <p>{t('homeedu.content1')}</p>
-                </div>
+                <a href="https://bit.ly/32L9VRT" target="_blank">
+                  <img
+                    className="lazyload"
+                    data-src={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img/img_7.png`}
+                    alt="Image"
+                  />
+                  <div className="edu_box_text">
+                    <p>{t('homeedu.content1')}</p>
+                  </div>
+                </a>
               </div>
             </div>
             <div className="col-lg-6 col-md-12">
               <div className="row">
                 <div className="col-lg-12 col-md-6 offset-md-3 offset-lg-0 col-sm-10 offset-sm-1">
                   <div className="edu_box">
-                    <img
-                      className="lazyload"
-                      data-src={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img/img_8.png`}
-                      alt="Image"
-                    />
-                    <div className="edu_box_text">
-                      <p>{t('homeedu.content2')}</p>
-                    </div>
+                    <a href="https://bit.ly/2RDqTuQ" target="_blank">
+                      <img
+                        className="lazyload"
+                        data-src={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img/img_8.png`}
+                        alt="Image"
+                      />
+                      <div className="edu_box_text">
+                        <p>{t('homeedu.content2')}</p>
+                      </div>
+                    </a>
                   </div>
                 </div>
                 <div className="col-lg-12 col-md-6 offset-md-3 offset-lg-0 col-sm-10 offset-sm-1">
                   <div className="edu_box">
-                    <img
-                      className="lazyload"
-                      data-src={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img/img_9.png`}
-                      alt="Image"
-                    />
-                    <div className="edu_box_text">
-                      <p>{t('homeedu.content3')}</p>
-                    </div>
+                    <a href="https://bit.ly/3hEL9ac" target="_blank">
+                      <img
+                        className="lazyload"
+                        data-src={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img/img_9.png`}
+                        alt="Image"
+                      />
+                      <div className="edu_box_text">
+                        <p>{t('homeedu.content3')}</p>
+                      </div>
+                    </a>
                   </div>
                 </div>
               </div>
