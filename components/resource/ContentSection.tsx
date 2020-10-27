21ab3096af311ed46917c58591ca206b317ab117
pachara <pachara.tho@1moby.com>
2020-10-27 16:18:10 +0700

    [Edit] Warngin code unused.

--- components/resource/ContentSection.tsx
+++ components/resource/ContentSection.tsx
@@ -1,38 +1,32 @@
-import { withTranslation } from '../../i18n';
 import PropTypes from 'prop-types';
-const ContentSection = ({  }: any) => (
+import { withTranslation } from '../../i18n';
+const ContentSection = () => (
   <div className="resource_section">
     <div className="container">
       <div className="row align-items-center">
         <div className="col-lg-12">
           <div className="secKnowledge">
-           <div className="headSection">
+            <div className="headSection">
               <h3>ความรู้</h3>
               <a href="">ดูทั้งหมด</a>
             </div>
             <div className="Knowledge">
               <div className="secArticle">
-                <div className="img-article">
-
-                </div>
+                <div className="img-article"></div>
                 <div className="text-article">
                   <h4>ห้ามพลาด!! เทรนด์การตลาด ปี 2020</h4>
                   <p>20-04-2020</p>
                 </div>
               </div>
               <div className="secArticle">
-                <div className="img-article">
-
-                </div>
+                <div className="img-article"></div>
                 <div className="text-article">
                   <h4>ห้ามพลาด!! เทรนด์การตลาด ปี 2020</h4>
                   <p>20-04-2020</p>
                 </div>
               </div>
               <div className="secArticle">
-                <div className="img-article">
-
-                </div>
+                <div className="img-article"></div>
                 <div className="text-article">
                   <h4>ห้ามพลาด!! เทรนด์การตลาด ปี 2020</h4>
                   <p>20-04-2020</p>
@@ -50,27 +44,21 @@ const ContentSection = ({  }: any) => (
               <a href="">ดูทั้งหมด</a>
             </div>
             <div className="hilight">
-              <div className="img-hilight">
-
-              </div>
+              <div className="img-hilight"></div>
               <div className="text-hilight">
                 <h4>ห้ามพลาด!! เทรนด์การตลาด ปี 2020</h4>
                 <p>20-04-2020</p>
               </div>
             </div>
             <div className="secArticle">
-               <div className="img-article">
-
-              </div>
+              <div className="img-article"></div>
               <div className="text-article">
                 <h4>ห้ามพลาด!! เทรนด์การตลาด ปี 2020</h4>
                 <p>20-04-2020</p>
               </div>
             </div>
             <div className="secArticle">
-               <div className="img-article">
-
-              </div>
+              <div className="img-article"></div>
               <div className="text-article">
                 <h4>ห้ามพลาด!! เทรนด์การตลาด ปี 2020</h4>
                 <p>20-04-2020</p>
@@ -79,33 +67,27 @@ const ContentSection = ({  }: any) => (
           </div>
         </div>
         <div className="col-lg-6 col-md-12">
-        <div className="secUsecase">
+          <div className="secUsecase">
             <div className="headSection">
               <h3>เรื่องราวความสำเร็จ</h3>
               <a href="">ดูทั้งหมด</a>
             </div>
             <div className="hilight">
-              <div className="img-hilight">
-
-              </div>
+              <div className="img-hilight"></div>
               <div className="text-hilight">
                 <h4>ห้ามพลาด!! เทรนด์การตลาด ปี 2020</h4>
                 <p>20-04-2020</p>
               </div>
             </div>
             <div className="secArticle">
-               <div className="img-article">
-
-              </div>
+              <div className="img-article"></div>
               <div className="text-article">
                 <h4>ห้ามพลาด!! เทรนด์การตลาด ปี 2020</h4>
                 <p>20-04-2020</p>
               </div>
             </div>
             <div className="secArticle">
-               <div className="img-article">
-
-              </div>
+              <div className="img-article"></div>
               <div className="text-article">
                 <h4>ห้ามพลาด!! เทรนด์การตลาด ปี 2020</h4>
                 <p>20-04-2020</p>
