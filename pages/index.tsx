a7644d301f52a5001ba490b83a8b16e17c9f926b
Apichart Khairavi <apichart@1moby.com>
2021-01-27 08:28:57 +0700

    change lazy load

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import Layout from '../components/Layout';
-// import Proloader from '../components/Proloader';
-import HeroSection from '../components/home/HeroSection';
-import PartnerSection from '../components/home/PartnerSection';
-import FeatureSection from '../components/home/FeatureSection';
-import SimpleSliderSection from '../components/home/SimpleSliderSection';
-import GlobeSection from '../components/home/GlobeSection';
-import SmsSection from '../components/home/SmsSection';
-import TestimonialSection from '../components/home/TestimonialSection';
-import EduSection from '../components/home/EduSection';
-import CtaSection from '../components/home/CtaSection';
+import dynamic from 'next/dynamic'
 import { withTranslation } from '../i18n';
-import BacktoTop from '../components/BacktoTop';
 import Head from 'next/head';
 import { NextSeo } from 'next-seo';
 import { seo } from '../components/seo/home';
+
+
+const Layout = dynamic(() => import('../components/Layout'))
+const HeroSection = dynamic(() => import('../components/home/HeroSection'))
+const PartnerSection = dynamic(() => import('../components/home/PartnerSection'))
+const FeatureSection = dynamic(() => import('../components/home/FeatureSection'))
+const SimpleSliderSection = dynamic(() => import('../components/home/SimpleSliderSection'))
+const GlobeSection = dynamic(() => import('../components/home/GlobeSection'))
+const SmsSection = dynamic(() => import('../components/home/SmsSection'))
+const TestimonialSection = dynamic(() => import('../components/home/TestimonialSection'))
+const EduSection = dynamic(() => import('../components/home/EduSection'))
+const CtaSection = dynamic(() => import('../components/home/CtaSection'))
+const BacktoTop = dynamic(() => import('../components/BacktoTop'))
 const Homepage = ({ t }: any) => (
   <React.Fragment>
     <Layout>
