e9096f66eb6593bdcc4af8cf7c46e247262862a1
Toon <phanuphong@1moby.com>
2021-07-21 17:24:19 +0700

    add new 3 page

---
+++ components/seo/lowprice.tsx
@@ -0,0 +1,18 @@
+export const seo = {
+    openGraph: {
+        url: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/lowprice`,
+        type: 'website',
+        title:
+            'บริการ OTP Ready-to-Use การส่งยืนยันตัวตนที่พร้อมใช้ทันที | ThaiBulkSMS',
+        description:
+            'บริการส่งข้อความยืนยันตัวตน OTP Ready-to-Use ที่พร้อมใช้ทันที ',
+        images: [
+            {
+                url: `${process.env.NEXT_PUBLIC_BASE_ASSET}/img/og-image/OG-TBS-Product-OTP.jpg`,
+                width: 800,
+                height: 600,
+            },
+        ],
+        site_name: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/lowprice`,
+    },
+};
---
+++ components/speed-and-reliable/HeroSection.tsx
@@ -0,0 +1,47 @@
+import { withTranslation } from '../../i18n';
+import PropTypes from 'prop-types';
+const HeroSectionV3 = ({ t }: any) => (
+    <div
+        className="hero_section v3 lazyload"
+        data-bgset={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img/bg_11.png`}
+    >
+        <div className="container">
+            <div className="row align-item-center hero_top_one">
+                <div className="col-lg-6 col-md-12 col-12">
+                    <div className="hero_img_one">
+                        <object
+                            type="image/svg+xml"
+                            data={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img/img-otp.svg`}
+                        ></object>
+                    </div>
+                </div>
+                <div className="col-lg-6 col-md-12 col-12">
+                    <div className="hero_text_one v2">
+                        <h1
+                            dangerouslySetInnerHTML={{
+                                __html: t(
+                                    'HeroSection::An <span>OTP transmission service</span> that is ready to use<br/>, increases security and does<br/> not require additional programming.'
+                                ),
+                            }}
+                        ></h1>
+                        <p
+                            dangerouslySetInnerHTML={{
+                                __html: t(
+                                    'HeroSection::This feature is only available for the <span>Corporate Package.</span>'
+                                ),
+                            }}
+                        ></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+HeroSectionV3.getInitialProps = async () => ({
+    namespacesRequired: ['Otp'],
+});
+
+HeroSectionV3.propTypes = {
+    t: PropTypes.func.isRequired,
+};
+export default withTranslation('Otp')(HeroSectionV3);
