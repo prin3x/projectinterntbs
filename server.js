d3bb46ecd1b6df8d0d09c02f979ddd5ab682411a
sirawit <sirawit@1moby.com>
2021-08-03 10:16:49 +0700

    hook url to remove trailingSlash and set nuxt config to false

--- server.js
+++ server.js
@@ -11,21 +11,30 @@ const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
 (async () => {
-  await app.prepare();
-  const server = express();
+    await app.prepare();
+    const server = express();
 
-  await nextI18next.initPromise;
-  server.use(bodyParser.urlencoded({ extended: true }));
-  server.use(nextI18NextMiddleware(nextI18next));
+    server.use((req, res, next) => {
+        const hasTrailingSlash = /\/$/.test(req.path);
+        if (req.path.length > 1 && hasTrailingSlash) {
+            const query = req.url.slice(req.path.length);
+            res.redirect(301, req.path.slice(0, -1) + query);
+        } else {
+            next();
+        }
+    });
 
-  server.get('*', (req, res) => handle(req, res));
-  server.post('/payment/callback', (req, res) => {
-    if (req.body.payment_status === '000')
-      res.redirect('/payment/thankyou');
-    else
-      res.redirect('/payment/cancel');
-  })
+    await nextI18next.initPromise;
+    server.use(bodyParser.urlencoded({ extended: true }));
+    server.use(nextI18NextMiddleware(nextI18next));
 
-  await server.listen(port);
-  console.log(`> Ready on http://localhost:${port}`); // eslint-disable-line no-console
+    server.get('*', (req, res) => handle(req, res));
+    server.post('/payment/callback', (req, res) => {
+        if (req.body.payment_status === '000')
+            res.redirect('/payment/thankyou');
+        else res.redirect('/payment/cancel');
+    });
+
+    await server.listen(port);
+    console.log(`> Ready on http://localhost:${port}`); // eslint-disable-line no-console
 })();
