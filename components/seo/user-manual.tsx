83d592208ead35eaa6438b0fe4ccb4930464b0f9
Toon <phanuphong@1moby.com>
2021-08-18 15:59:54 +0700

    add new feature/add-user-manual-page

---
+++ components/seo/user-manual.tsx
@@ -0,0 +1,18 @@
+export const seo = {
+    openGraph: {
+        url: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/product/user-manual`,
+        type: 'website',
+        title:
+            'รวมข้อมูลและคู่มือที่จะทำให้ Developer เริ่มต้นใช้งาน API ได้รวดเร็วยิ่งขึ้น | ThaiBulkSMS',
+        description:
+            'รวบรวมคู่มือที่เป็นประโยชน์สำหรับนักพัฒนา เพื่อเป็นแนวทางในการเขียนโปรแกรม',
+        images: [
+            {
+                url: `${process.env.NEXT_PUBLIC_BASE_ASSET}/img/og-image/OG-TBS-Home.jpg`,
+                width: 800,
+                height: 600,
+            },
+        ],
+        site_name: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/product/user-manual`,
+    },
+};
