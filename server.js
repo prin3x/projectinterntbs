77c5c178db9320fea644406d7e707eee6bd28d55
wndererk <bangwan.ap@hotmail.com>
2021-09-23 03:12:17 +0700

    Edit HeaderSubmodule.js and FooterSubmodule.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const next = require('next');
-const nextI18NextMiddleware = require('next-i18next/middleware').default;
+// const nextI18NextMiddleware = require('next-i18next/middleware').default;
 
-const nextI18next = require('./i18n');
+// const nextI18next = require('./i18n');
 
 const port = process.env.PORT || 3000;
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
@@ -14,9 +14,9 @@ const handle = app.getRequestHandler();
     await app.prepare();
     const server = express();
 
-    await nextI18next.initPromise;
-    server.use(bodyParser.urlencoded({ extended: true }));
-    server.use(nextI18NextMiddleware(nextI18next));
+    // await nextI18next.initPromise;
+    // server.use(bodyParser.urlencoded({ extended: true }));
+    // server.use(nextI18NextMiddleware(nextI18next));
 
     server.get('*', (req, res) => handle(req, res));
     server.post('/payment/callback', (req, res) => {
