378991c63c08cdcbd93596d968d1bc6e4c8a1d45
wndererk <bangwan.ap@hotmail.com>
2021-10-06 09:32:08 +0700

    first commit

--- components/product/SmsSection.tsx
+++ components/product/SmsSection.tsx
@@ -3,7 +3,7 @@ import th from '../../public/locales/th/Product.json';
 import en from '../../public/locales/en/Product.json';
 // import PropTypes from 'prop-types';
 import TestQuickregister from '../quickregister/testQuickregister';
-// import 'react-modal-video/scss/modal-video.scss';
+import 'react-modal-video/scss/modal-video.scss';
 import ModalVideo from 'react-modal-video';
 import { useState } from 'react';
 import Cookie from 'js-cookie';
--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@
 //     plugins: [],
 // });
 
+
 module.exports = {
     reactStrictMode: true,
     trailingSlash: true,
